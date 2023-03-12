4e0cbe4a5c3b045d5ec2665964cf8d23d995cf6f
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-03-12 13:47:25 +0530

    Revert "i dont know"
    This reverts commit faef820e88efbf9b73f20b190ad59f48f0f8744f.

--- front-end/app/create/page.tsx
+++ front-end/app/create/page.tsx
@@ -7,7 +7,6 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
-import DatePicker from '@/app/create/DatePicker';
 
 // TODO Fetch Clubs in this way
 const clubs = [
@@ -39,49 +38,17 @@ export default function Create() {
 	const [club, setClub] = useState('');
 	const [checked, setChecked] = React.useState(false);
 
-	const [image, setImage] = useState<string | string>('');
-	const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-		const file = e.target.files?.[0];
-		if (!file) {
-			return;
-		}
-		const reader = new FileReader();
-		reader.onload = () => {
-			setImage(reader.result as string);
-		};
-		reader.readAsDataURL(file);
-	};
-
-	const [description, setDescription] = useState('');
-
-	function handleDescriptionChange(event: any) {
-		setDescription(event.target.value);
-	}
-
-	const [startDate, setStartDate] = useState('');
-	const [endDate, setEndDate] = useState('');
-
 	return (
-		<div className='flex flex-col w-full justify-center items-center gap-4'>
-			<h1 className='text-2xl text-center underline mt-3'>Create your Event</h1>
-			<div className='flex flex-col bg-red-100 mx-2 rounded-md p-3 lg:hidden'>
-				<h1 className='text-xl underline font-semibold'>Note:</h1>
-				<p>
-					For optimal results in visualizing the final appearance of your card on the
-					portal, we highly recommend utilizing a personal computer or laptop in
-					full-screen mode during the creation or editing process. This will allow
-					you to have a comprehensive preview of your card's layout, design, and
-					other details before it goes live on the portal.
-				</p>
-			</div>
-			<div className='flex flex-row w-full px-4'>
-				<div className='flex flex-col w-full mx-5 gap-4'>
+		<div className="flex flex-col w-full justify-center items-center gap-4">
+			<h1 className="text-2xl text-center underline mt-3">Create your Event</h1>
+			<div className="flex flex-row w-full px-4">
+				<div className="flex flex-col w-full mx-5 gap-3">
 					<TextField
-						autoComplete='off'
+						autoComplete="off"
 						required
-						id='outlined-required'
-						label='Event Name Here'
-						defaultValue=''
+						id="outlined-required"
+						label="Event Name Here"
+						defaultValue=""
 						fullWidth
 						onChange={handleEventNameChange}
 						value={eventName}
@@ -90,7 +57,7 @@ export default function Create() {
 					<Autocomplete
 						{...defaultProps}
 						disabled={checked === true}
-						id='disable-clearable'
+						id="disable-clearable"
 						disableClearable
 						value={value}
 						onChange={(event, newValue) => {
@@ -101,14 +68,14 @@ export default function Create() {
 							<TextField
 								{...params}
 								required
-								label='Organiser/Club Name Here'
-								variant='outlined'
+								label="Organiser/Club Name Here"
+								variant="outlined"
 								helperText="Check our list first. If your club or organiser's name is not included, check the box below to manually enter it."
 							/>
 						)}
 					/>
 					<FormControlLabel
-						className='ml-3'
+						className="ml-3"
 						disabled={club !== ''}
 						control={
 							<Checkbox
@@ -116,20 +83,20 @@ export default function Create() {
 								onChange={() => {
 									setChecked(!checked);
 								}}
-								className='w-max'
+								className="w-max"
 								inputProps={{'aria-label': 'controlled'}}
 							/>
 						}
-						label='I assure you that my club or organiser name was not found in the above list.'
+						label="I assure you that my club or organiser name was not found in the above list."
 					/>
 					{checked ? (
 						<TextField
 							required
-							autoComplete='off'
+							autoComplete="off"
 							disabled={!checked}
-							id='outlined-required'
-							label='New Organiser/Club Name Here'
-							defaultValue=''
+							id="outlined-required"
+							label="New Organiser/Club Name Here"
+							defaultValue=""
 							onChange={(event) => {
 								setClub(event.target.value);
 							}}
@@ -137,101 +104,25 @@ export default function Create() {
 							helperText="Double check for typos while entering your club/organiser name in the list above. If you still can't find it, proceed by manually entering your club/organiser name."
 						/>
 					) : null}
-					<Button variant='outlined' component='label' className='w-auto max-w-xl'>
-						Upload your Event Poster Here*
-						<input
-							hidden
-							accept='image/*'
-							multiple
-							type='file'
-							onChange={handleImageUpload}
-							required
-						/>
+					<Button variant="outlined" component="label" className="w-auto max-w-xl">
+						Upload your Event Poster
+						<input hidden accept="image/*" multiple type="file" required />
 					</Button>
-					<p className='-mt-2 text-xs text-gray-500 ml-3'>
+					<p className="-mt-2 text-xs text-gray-500 ml-3">
 						Event Poster will be displayed on both the homepage cards and the event's
 						details page.
 					</p>
-					<DatePicker setStartDate={setStartDate} setEndDate={setEndDate} />
-					<TextField
-						autoComplete='off'
-						required
-						id='outlined-required'
-						label='Short Description Here'
-						fullWidth
-						onChange={handleDescriptionChange}
-						value={description}
-						helperText="This will be reflected on the homepage cards and the event's details page header."
-						inputProps={{maxLength: 100}}
-					/>
-					<TextField
-						id='outlined-multiline-flexible'
-						label='Long Description Here'
-						name='longDescription'
-						multiline
-						minRows={4}
-						helperText='Please provide a detailed explanation of the event in this field. Include any important details or context about the event here.'
-					/>
-					{/* Branch */}
-					<Autocomplete
-						multiple
-						id='tags-outlined'
-						options={matchedList}
-						getOptionLabel={(option) => option.name}
-						filterSelectedOptions
-						renderInput={(params) => (
-							<TextField
-								{...params}
-								label="Event's Branches"
-								placeholder='Select the branches that the Event is open to..'
-							/>
-						)}
-					/>
-					<TextField
-						id='outlined-multiline-flexible'
-						label='Date - Detailed'
-						name='dateDetail'
-						multiline
-						minRows={2}
-						helperText="Please specify the dates for each sub-event(optional) in the text field above. This will be displayed on the event's details page."
-					/>
-					<TextField
-						id='outlined-multiline-flexible'
-						label='Duration Here'
-						name='duration'
-						multiline
-						minRows={2}
-						helperText='Please provide specific instructions regarding the timing of the event, including the duration and the time at which students are required to report to the venue.'
-					/>
-					<TextField
-						autoComplete='off'
-						id='outlined-required'
-						label='Venue Here'
-						defaultValue=''
-						fullWidth
-						name='venue'
-						helperText='Venue is optional.'
-					/>
 				</div>
-				<div className='hidden lg:block'>
-					<div className='flex flex-col gap-2 items-center justify-center bg-blue-50 p-3 rounded-md'>
-						<p className='text-2xl underline'>Preview</p>
+				<div className="hidden lg:block">
+					<div className="flex flex-col gap-2 items-center justify-center bg-blue-50 p-3 rounded-md">
+						<p className="text-2xl underline">Preview</p>
 						<Card
-							title={eventName || 'Event\'s Name Here'}
-							subheader={club || 'Organiser/Club\'s Name Here'}
-							imageUrl={image || 'pulseLoading'} // pulseLoading shows the pulse loader
-							description={description || 'Short Description Here'}
-							date={
-								startDate === endDate && startDate !== ''
-									? startDate
-									: startDate && endDate
-										? startDate + ' - ' + endDate
-										: startDate
-											? startDate
-											: 'Event\'s Date Here'
-							}
-							learnMoreLink='/create'
-							hover={false}></Card>
+							title={eventName === '' ? 'Event Name Here' : eventName}
+							subheader={club === '' ? 'Organiser/Club Name Here' : club}
+							imageUrl="/eventPosters/Madhugai.jpg"
+							description="Event Short Description Here"
+							date="Date of the Event Here"
+							learnMoreLink="/create"></Card>
 					</div>
 				</div>
 			</div>
