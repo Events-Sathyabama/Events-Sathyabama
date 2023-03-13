3cd157fdf1489814ed50d547858bf8ff44b10c88
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-03-13 10:31:39 +0530

    worked on create form

--- front-end/app/create/page.tsx
+++ front-end/app/create/page.tsx
@@ -15,10 +15,33 @@ import Chip from '@mui/material/Chip';
 import API from '../API';
 
 const axios = new API.Axios();
+// TODO Fetch Clubs in this way
+const clubs = [
+	{name: 'Music Club'},
+	{name: 'Dance Club'},
+	{name: 'ACM'},
+	{name: 'Google Developers Student Club'},
+	{name: 'Community Development Club'},
+];
+
+// TODO Fetch Branches in this way
+const matchedList = [
+	{name: 'CSE 2020-2024'},
+	{name: 'CSE 2021-2025'},
+	{name: 'CSE 2022-2026'},
+];
 
 interface ClubsType {
 	name: string;
 }
+
+// TODO Fetch Coordinators in this way
+const Coordinators = [
+	{name: 'Bandepalli Surya', id: '40110156'},
+	{name: 'Aryan Amish', id: '4011022'},
+	{name: 'Dr. Revathy', id: '11'},
+	{name: 'John King', id: '41101022'},
+];
 
 export default function Create() {
 	// API calls starts
@@ -99,13 +122,26 @@ export default function Create() {
 		setEndDate(day + ' ' + month.substring(0, 3) + " '" + year);
 	};
 
-	const fixedOptions = [top100Films[6]];
-	const [coval, setCoval] = React.useState([...fixedOptions, top100Films[13]]);
+	const fixedOptions = [Coordinators[0]];
+	const [coval, setCoval] = React.useState([...fixedOptions]);
+
+	const handleSubmit = async (event: any) => {
+		event.preventDefault();
+		try {
+			console.log('Submitting');
+			// submit here all params
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<div className="flex flex-col w-full justify-center items-center gap-4 px-2">
 			<h1 className="text-2xl text-center underline mt-3">Create/Edit your Event</h1>
-			<div className="flex flex-col md:flex-row w-full sm:px-4 gap-2 items-center md:items-start">
-				<div className="flex flex-col w-full mx-5 gap-4 max-w-sm md:max-w-full">
+			<div className="flex flex-col md:flex-row w-full sm:px-4 gap-4 md:gap-0 items-center md:items-start">
+				<form
+					onSubmit={handleSubmit}
+					className="flex flex-col w-full mx-5 gap-4 max-w-sm md:max-w-full">
 					<TextField
 						autoComplete="off"
 						required
@@ -258,20 +294,26 @@ export default function Create() {
 								...fixedOptions,
 								...newValue.filter((option) => fixedOptions.indexOf(option) === -1),
 							]);
+							console.log(newValue);
 						}}
-						options={top100Films}
-						getOptionLabel={(option) => option.title}
+						options={Coordinators}
+						getOptionLabel={(option) => `${option.name} - ${option.id}`}
 						renderTags={(tagValue, getTagProps) =>
 							tagValue.map((option, index) => (
 								<Chip
-									label={option.title}
+									label={option.name + ' - ' + option.id}
 									{...getTagProps({index})}
 									disabled={fixedOptions.indexOf(option) !== -1}
 								/>
 							))
 						}
 						renderInput={(params) => (
-							<TextField {...params} label="Fixed tag" placeholder="Favorites" />
+							<TextField
+								required
+								{...params}
+								label="Faculty and Student Coordinators Here"
+								placeholder="Name, Employee ID or Register Number Here"
+							/>
 						)}
 					/>
 					<TextField
@@ -299,7 +341,14 @@ export default function Create() {
 						name="venue"
 						helperText="Venue is optional."
 					/>
-				</div>
+					<p className="text-xs text-gray-500 ml-3 md:hidden">
+						Please review the preview of your event's homepage card below and click
+						submit.
+					</p>
+					<Button type="submit" variant="contained" className="bg-blue-500">
+						Submit Event For Review
+					</Button>
+				</form>
 				<div className="block max-w-sm md:max-w-fit">
 					<div className="flex flex-col gap-2 items-center justify-center bg-blue-50 p-3 rounded-md">
 						<p className="text-2xl underline">Preview</p>
@@ -325,131 +374,3 @@ export default function Create() {
 		</div>
 	);
 }
-
-// TODO Fetch Coordinators in this way
-const top100Films = [
-	{title: 'The Shawshank Redemption', year: 1994},
-	{title: 'The Godfather', year: 1972},
-	{title: 'The Godfather: Part II', year: 1974},
-	{title: 'The Dark Knight', year: 2008},
-	{title: '12 Angry Men', year: 1957},
-	{title: "Schindler's List", year: 1993},
-	{title: 'Pulp Fiction', year: 1994},
-	{
-		title: 'The Lord of the Rings: The Return of the King',
-		year: 2003,
-	},
-	{title: 'The Good, the Bad and the Ugly', year: 1966},
-	{title: 'Fight Club', year: 1999},
-	{
-		title: 'The Lord of the Rings: The Fellowship of the Ring',
-		year: 2001,
-	},
-	{
-		title: 'Star Wars: Episode V - The Empire Strikes Back',
-		year: 1980,
-	},
-	{title: 'Forrest Gump', year: 1994},
-	{title: 'Inception', year: 2010},
-	{
-		title: 'The Lord of the Rings: The Two Towers',
-		year: 2002,
-	},
-	{title: "One Flew Over the Cuckoo's Nest", year: 1975},
-	{title: 'Goodfellas', year: 1990},
-	{title: 'The Matrix', year: 1999},
-	{title: 'Seven Samurai', year: 1954},
-	{
-		title: 'Star Wars: Episode IV - A New Hope',
-		year: 1977,
-	},
-	{title: 'City of God', year: 2002},
-	{title: 'Se7en', year: 1995},
-	{title: 'The Silence of the Lambs', year: 1991},
-	{title: "It's a Wonderful Life", year: 1946},
-	{title: 'Life Is Beautiful', year: 1997},
-	{title: 'The Usual Suspects', year: 1995},
-	{title: 'Léon: The Professional', year: 1994},
-	{title: 'Spirited Away', year: 2001},
-	{title: 'Saving Private Ryan', year: 1998},
-	{title: 'Once Upon a Time in the West', year: 1968},
-	{title: 'American History X', year: 1998},
-	{title: 'Interstellar', year: 2014},
-	{title: 'Casablanca', year: 1942},
-	{title: 'City Lights', year: 1931},
-	{title: 'Psycho', year: 1960},
-	{title: 'The Green Mile', year: 1999},
-	{title: 'The Intouchables', year: 2011},
-	{title: 'Modern Times', year: 1936},
-	{title: 'Raiders of the Lost Ark', year: 1981},
-	{title: 'Rear Window', year: 1954},
-	{title: 'The Pianist', year: 2002},
-	{title: 'The Departed', year: 2006},
-	{title: 'Terminator 2: Judgment Day', year: 1991},
-	{title: 'Back to the Future', year: 1985},
-	{title: 'Whiplash', year: 2014},
-	{title: 'Gladiator', year: 2000},
-	{title: 'Memento', year: 2000},
-	{title: 'The Prestige', year: 2006},
-	{title: 'The Lion King', year: 1994},
-	{title: 'Apocalypse Now', year: 1979},
-	{title: 'Alien', year: 1979},
-	{title: 'Sunset Boulevard', year: 1950},
-	{
-		title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-		year: 1964,
-	},
-	{title: 'The Great Dictator', year: 1940},
-	{title: 'Cinema Paradiso', year: 1988},
-	{title: 'The Lives of Others', year: 2006},
-	{title: 'Grave of the Fireflies', year: 1988},
-	{title: 'Paths of Glory', year: 1957},
-	{title: 'Django Unchained', year: 2012},
-	{title: 'The Shining', year: 1980},
-	{title: 'WALL·E', year: 2008},
-	{title: 'American Beauty', year: 1999},
-	{title: 'The Dark Knight Rises', year: 2012},
-	{title: 'Princess Mononoke', year: 1997},
-	{title: 'Aliens', year: 1986},
-	{title: 'Oldboy', year: 2003},
-	{title: 'Once Upon a Time in America', year: 1984},
-	{title: 'Witness for the Prosecution', year: 1957},
-	{title: 'Das Boot', year: 1981},
-	{title: 'Citizen Kane', year: 1941},
-	{title: 'North by Northwest', year: 1959},
-	{title: 'Vertigo', year: 1958},
-	{
-		title: 'Star Wars: Episode VI - Return of the Jedi',
-		year: 1983,
-	},
-	{title: 'Reservoir Dogs', year: 1992},
-	{title: 'Braveheart', year: 1995},
-	{title: 'M', year: 1931},
-	{title: 'Requiem for a Dream', year: 2000},
-	{title: 'Amélie', year: 2001},
-	{title: 'A Clockwork Orange', year: 1971},
-	{title: 'Like Stars on Earth', year: 2007},
-	{title: 'Taxi Driver', year: 1976},
-	{title: 'Lawrence of Arabia', year: 1962},
-	{title: 'Double Indemnity', year: 1944},
-	{
-		title: 'Eternal Sunshine of the Spotless Mind',
-		year: 2004,
-	},
-	{title: 'Amadeus', year: 1984},
-	{title: 'To Kill a Mockingbird', year: 1962},
-	{title: 'Toy Story 3', year: 2010},
-	{title: 'Logan', year: 2017},
-	{title: 'Full Metal Jacket', year: 1987},
-	{title: 'Dangal', year: 2016},
-	{title: 'The Sting', year: 1973},
-	{title: '2001: A Space Odyssey', year: 1968},
-	{title: "Singin' in the Rain", year: 1952},
-	{title: 'Toy Story', year: 1995},
-	{title: 'Bicycle Thieves', year: 1948},
-	{title: 'The Kid', year: 1921},
-	{title: 'Inglourious Basterds', year: 2009},
-	{title: 'Snatch', year: 2000},
-	{title: '3 Idiots', year: 2009},
-	{title: 'Monty Python and the Holy Grail', year: 1975},
-];
