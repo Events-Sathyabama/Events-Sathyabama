d0e28512d6c20782868504f41501afebab478420
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-05-29 13:14:44 +0530

    fixed completed tab

--- front-end/app/profile/profileCards.tsx
+++ front-end/app/profile/profileCards.tsx
@@ -75,7 +75,23 @@ export default function ProfileCards(props: any) {
 						) : (
 							<div className="flex flex-row gap-2 justify-center items-center shadow-sm border border-gray-300 w-36 py-2 rounded-md ml-1">
 								{(() => {
-									if (
+									if (props.eventStatus === 'Completed') {
+										return (
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth={1.5}
+												stroke="currentColor"
+												className="w-6 h-6 text-green-500">
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+												/>
+											</svg>
+										);
+									} else if (
 										props.eventStatus === 'Pending' ||
 										applicationStatus === 'Pending'
 									) {
@@ -129,22 +145,6 @@ export default function ProfileCards(props: any) {
 													strokeLinecap="round"
 													strokeLinejoin="round"
 													d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-												/>
-											</svg>
-										);
-									} else if (props.eventStatus === 'Completed') {
-										return (
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												strokeWidth={1.5}
-												stroke="currentColor"
-												className="w-6 h-6 text-green-500">
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 												/>
 											</svg>
 										);
