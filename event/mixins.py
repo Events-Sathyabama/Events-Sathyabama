41f296f1c95668256e832c590e28978f1b562805
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-06-14 22:05:07 +0530

    fixed the multiple event bug

--- event/mixins.py
+++ event/mixins.py
@@ -6,20 +6,21 @@ class SearchQueryMixins:
     def get_queryset(self, *args, **kwargs):
         search = self.request.GET.get('q')
         search = search.strip() if search is not None else search
-        query = Event.objects.all()
+        search = None if search == '' else search
+
         q = Q(start_date__isnull=False)
         q = q | Q(end_date__isnull=False)
         q = q & Q(status__gte=4, hod_verified=1, dean_verified=1, vc_verified=1) & ~Q(status=9)
         if search is None:
-            return query.filter(q)
-        query = query.filter(q)
+            return Event.objects.filter(q)
         search = search.split(" ")
-        q = Q()
+        search_q = Q()
         for s in search:
-            q = q | (Q(title__icontains=s) | 
+            if s == '': continue
+            search_q = search_q | (Q(title__icontains=s) | 
                 Q(short_description__icontains=s) | 
                 Q(long_description__icontains=s) | 
                 Q(club__icontains=s) | 
                 Q(branch__name__icontains=s)
                 )
-        return query.filter(q)
+        return Event.objects.filter(q & search_q)
