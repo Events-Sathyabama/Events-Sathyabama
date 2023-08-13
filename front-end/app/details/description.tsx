d58d20169ffd163462a05eb9e90af9b394d16f76
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-08-13 20:15:49 +0530

    Fixed Description Text Warp

--- front-end/app/details/description.tsx
+++ front-end/app/details/description.tsx
@@ -1,7 +1,7 @@
 export default function Description(props: {long_desc: string | undefined}) {
 	return (
 		<div className="w-full border border-gray-300 rounded-md">
-			<p className="p-4">{props.long_desc}</p>
+			<pre className="p-4 font-roboto">{props.long_desc}</pre>
 		</div>
 	);
 }
