1f28b764d6b94942669f32681b6fffeba052f4db
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-04 14:28:47 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,8 @@ echo "Starting Build"
 python3 --version
 python --version
 pip3 --version
-sudo apt-get install python-mysqldb 
-sudo apt-get install libmysqlclient-dev
+apt-get install python-mysqldb 
+apt-get install libmysqlclient-dev
 pip3 install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
