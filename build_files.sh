2be86c1910b3a72b88983154d29b91cc33411ff7
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-04 14:28:14 +0530

    trying to fix mysql error

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,8 @@ echo "Starting Build"
 python3 --version
 python --version
 pip3 --version
+sudo apt-get install python-mysqldb 
+sudo apt-get install libmysqlclient-dev
 pip3 install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
