7525f100da503f090aaf79179dbe5d0472320e90
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-04 14:20:37 +0530

    trying new python version

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,8 @@
 echo "Starting Build"
 python3 --version
-python3.10 --version
 python --version
-pip --version
-pip install -r requirements.txt
-python3.10 manage.py migrate
-python3.10 manage.py collectstatic --noinput
+pip3 --version
+pip3 install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 echo "Build End"
