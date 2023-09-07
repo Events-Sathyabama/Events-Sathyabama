2a49e5642e6b2ebb167f754a25df1d6626b259fe
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-07 15:43:53 +0530

    added comments and some image information command

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,20 @@
 echo "Starting Build"
+echo "Image Information"
+cat /etc/os-release
+lsb_release -a
+uname -r
+hostnamectl
+echo "End of image information"
+
+echo "Installing Linux Packages"
 yum install git -y
 yum install gcc -y
 yum install libpq-devel -y
+echo "Done installing Linux Packages"
+
+echo "Settings Up Python Environment"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py migrate
+echo "All Good to go"
 echo "Build End"
