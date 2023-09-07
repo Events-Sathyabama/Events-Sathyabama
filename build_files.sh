f1d67b585cac64d5de98848e3abb0bc6bf814c57
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-07 15:41:10 +0530

    remove yum and added dnf

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 echo "Starting Build"
-yum install libpq-devel -y
-yum install gcc -y
+dnf install git -y
+dnf install gcc -y
+dnf install libpq-devel -y
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py migrate
