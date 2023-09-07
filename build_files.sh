75802229c58a3c68caa8048a028d2274983a7e1d
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-07 15:33:22 +0530

    updated build_sh file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "Starting Build"
+yum install libpq-devel -y
+yum install gcc -y
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py migrate
