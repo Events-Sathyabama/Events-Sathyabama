9baca646d137e5c635bb18335b521fad5afd08a3
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-07 15:41:44 +0530

    dnf is not install in vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo "Starting Build"
-dnf install git -y
-dnf install gcc -y
-dnf install libpq-devel -y
+yum install git -y
+yum install gcc -y
+yum install libpq-devel -y
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py migrate
