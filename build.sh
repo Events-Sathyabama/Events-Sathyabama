3bd897cb68a473ed97c1795acd744f723fc95da8
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-09 16:42:03 +0530

    fixed type in the work True in build_script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 python manage.py migrate
 python manage.py collectstatic --noinput --clear
 
-if [ -z "$MYSQL_DATABASE" ] && [ -z "$POSTGRES_DATABASE" ] && [ "${MYSQL_DATABASE,,}" != "true" ] && [ "${POSTGRES_DATABASE,,}" != "true" ]; then
+if [ -z "$MYSQL_DATABASE" ] && [ -z "$POSTGRES_DATABASE" ] && [ "${MYSQL_DATABASE,,}" != "True" ] && [ "${POSTGRES_DATABASE,,}" != "True" ]; then
     # Run the createsuperuser command with the desired parameters
     python manage.py createsuperuser createsuperuser --college_id=0 --email=aryanamish385@gmail.com --role=4 --noinput
     python manage.py createsuperuser createsuperuser --college_id=1 --email=bsuryakumar03@gmail.com --role=4 --noinput
