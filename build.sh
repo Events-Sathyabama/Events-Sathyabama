6d843048fb4bb42baaac8b22c2ffc410f7cb475b
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-09-09 16:50:51 +0530

    trying to fix the bash script

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 python manage.py migrate
 python manage.py collectstatic --noinput --clear
-echo "MYSQL_DATABASE: $MYSQL_DATABASE"
-echo "POSTGRES_DATABASE: $POSTGRES_DATABASE"
-if [ -z "$MYSQL_DATABASE" ] && [ -z "$POSTGRES_DATABASE" ] && [ "${MYSQL_DATABASE,,}" != "True" ] && [ "${POSTGRES_DATABASE,,}" != "True" ]; then
+echo "MYSQL_DATABASE: ${MYSQL_DATABASE,,}"
+echo "POSTGRES_DATABASE: ${POSTGRES_DATABASE,,}"
+if [ -z "$MYSQL_DATABASE" ] && [ -z "$POSTGRES_DATABASE" ]; then
+    if [ "${MYSQL_DATABASE,,}" != "true" ] || [ "${POSTGRES_DATABASE,,}" != "true" ]; then
     # Run the createsuperuser command with the desired parameters
     python manage.py createsuperuser createsuperuser --college_id=0 --email=aryanamish385@gmail.com --role=4 --noinput
     python manage.py createsuperuser createsuperuser --college_id=1 --email=bsuryakumar03@gmail.com --role=4 --noinput
+    fi
 else
     echo "MYSQL_DATABASE and POSTGRES_DATABASE are set or at least one of them is set to true, so the superuser creation is skipped."
 fi
