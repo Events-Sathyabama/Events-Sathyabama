8516f8041c4e8c0a1059d4533c5c3b7a29deda31
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-11-12 05:03:29 +0530

    changed static files root path

--- event_management/settings.py
+++ event_management/settings.py
@@ -207,7 +207,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage' if check_cdn() else 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
